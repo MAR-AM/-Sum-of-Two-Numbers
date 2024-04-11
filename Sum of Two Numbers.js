4556087a53763ef2e8edd8e48d2af4c407f780c4
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-11 00:51:37 +0000

    Create Sum of Two Numbers.js

---
+++ Sum of Two Numbers.js
@@ -0,0 +1,7 @@
+function calculateSum(a, b) {
+            return a + b;
+        }
+        var num1 = parseFloat(prompt("Enter the first number: "));
+        var num2 = parseFloat(prompt("Enter the second number: "));
+        var sum = calculateSum(num1, num2);
+        document.write("The sum of the two entered numbers is: " + sum + "<br>");
